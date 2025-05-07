7f4a7c271e0718a49c7f5bc7bf2f7a25c54da9b0
Luke Hamilton <pies.eth@pm.me>
2025-05-07 16:43:49 -0400

    feat: change RD price calc to use stableswap math

--- packages/contracts/contracts/Interfaces/IOracle.sol
+++ packages/contracts/contracts/Interfaces/IOracle.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.24;
  * @notice Interface for the Oracle hook contract
  */
 interface IOracle {
+    // --- Events ---
+
     // --- Errors ---
 
     /**
@@ -18,11 +20,51 @@ interface IOracle {
      */
     error Oracle_PoolNotFromFactory(address pool);
 
-    // --- Data ---
+    /**
+     * @notice Error thrown when trying to divide by zero
+     */
+    error Oracle_DivisionByZero();
+
+    // --- Registry ---
 
     /**
-     * @notice Getter for the pool address
+     * @notice Getter for the balancer v3 pool address
      * @return _pool The pool address
      */
     function pool() external view returns (address _pool);
+
+    /**
+     * @notice Getter for the balancer v3 vault address
+     * @return _vault The vault address
+     */
+    function vault() external view returns (address _vault);
+
+    /**
+     * @notice Getter for the RD token address
+     * @return _rdToken The RD token address
+     */
+    function rdToken() external view returns (address _rdToken);
+
+    /**
+     * @notice Getter for the stablecoin basket
+     * @return _stablecoinBasket The stablecoin basket
+     */
+    function stablecoinBasket() external view returns (address[] memory _stablecoinBasket);
+
+    // --- Data ---
+
+    /**
+     * @notice Getter for the RD token index
+     * @return _rdTokenIndex The RD token index
+     */
+    function rdTokenIndex() external view returns (uint8 _rdTokenIndex);
+
+    /**
+     * @notice Getter for the stablecoin basket indices
+     * @return _stablecoinBasketIndices The stablecoin basket indices
+     */
+    function stablecoinBasketIndices()
+        external
+        view
+        returns (uint8[] memory _stablecoinBasketIndices);
 }
--- packages/contracts/contracts/Oracle.sol
+++ packages/contracts/contracts/Oracle.sol
@@ -14,29 +14,50 @@ import {VaultGuard} from "./Vendor/balancer-v3/vault/contracts/VaultGuard.sol";
 import {HookFlags, TokenConfig, LiquidityManagement, AfterSwapParams} from "./Vendor/balancer-v3/interfaces/contracts/vault/VaultTypes.sol";
 
 import {BasePoolFactory} from "./Vendor/balancer-v3/pool-utils/contracts/BasePoolFactory.sol";
+import {StablePool, Rounding} from "./Vendor/balancer-v3/pool-stable/contracts/StablePool.sol";
 
+import {FixedPoint} from "./Vendor/balancer-v3/solidity-utils/contracts/math/FixedPoint.sol";
 import {IOracle} from "./Interfaces/IOracle.sol";
 
 // Note: If > 50% of tokens in pool are yield bearing must use rate provider for token
 //  https://docs.balancer.fi/partner-onboarding/onboarding-overview/rate-providers.html
 
 contract Oracle is IOracle, BaseHooks, VaultGuard {
+    using FixedPoint for uint256;
+
+    /// @inheritdoc IOracle
+    uint256 public constant WAD = 1e18;
+
     // --- Registry ---
+
+    /// @inheritdoc IOracle
     address public pool;
 
-    address public constant AGGREGATOR_ROUTER = 0x309abcAeFa19CA6d34f0D8ff4a4103317c138657;
+    /// @inheritdoc IOracle
+    address public vault;
+
+    /// @inheritdoc IOracle
+    address public rdToken;
+
+    /// @inheritdoc IOracle
+    address[] public stablecoinBasket;
 
     // --- Data ---
-    address public USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address public USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-    address public DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address public RD = 0x0000000000000000000000000000000000000000;
 
-    address USDC_CL_FEED = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
-    address USDT_CL_FEED = 0x3E7d1eAB13ad0104d2750B8863b489D65364e32D;
-    address DAI_CL_FEED = 0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9;
+    /// @inheritdoc IOracle
+    uint8 public rdTokenIndex;
 
-    constructor(IVault vault) VaultGuard(vault) {}
+    /// @inheritdoc IOracle
+    uint8[] public stablecoinBasketIndices;
+
+    constructor(
+        IVault _vault,
+        address _rdToken,
+        address[] memory _stablecoinBasket
+    ) VaultGuard(_vault) {
+        rdToken = _rdToken;
+        stablecoinBasket = _stablecoinBasket;
+    }
 
     /// @inheritdoc IHooks
     function onRegister(
@@ -64,101 +85,192 @@ contract Oracle is IOracle, BaseHooks, VaultGuard {
     }
 
     function onAfterSwap(
-        AfterSwapParams calldata params_
-    ) public override onlyVault returns (bool, uint256) {
+        AfterSwapParams calldata _params
+    ) public override onlyVault returns (bool _success, uint256 _hookAdjustedAmountCalculatedRaw) {
         // If time since last observation > minDelta then update price in observations
-        _updatePrice();
+        bool _shouldUpdate = true;
+
+        if (_shouldUpdate) {
+            // Get last balances of all tokens in the pool
+            (, , , uint256[] memory _lastBalancesWad) = IVault(vault).getPoolTokenInfo(_params.pool);
+            _updateSyntheticRDPrice(_lastBalancesWad);
+        }
+
         return (true, 0);
     }
 
     // Get's RD price and saves it to the observations
-    function _updatePrice() internal {
+    function _updateSyntheticRDPrice(uint256[] memory _lastBalancesWad) internal {
         // TODO: Implement price update
+        uint256 _currentSyntheticRDPriceWad = _calculateInstantaneousSyntheticRDPrice(
+            _lastBalancesWad
+        );
     }
 
-    // Get RD price
-    function _getPrice() internal returns (uint256) {
-        // 1. Get price of RD in terms of each other token in the pool (# of base units of USDC received for 1 base unit of RD)
-        // This is the balancer v3 router
-        IAggregatorRouter aggregatorRouter = IAggregatorRouter(AGGREGATOR_ROUTER);
+    /**
+     * @notice Get the instantaneous synthetic RD price
+     * @param  _lastBalancesWad The last balances of the pool
+     * @return _syntheticRDPriceWad The instantaneous synthetic RD price
+     */
+    function _calculateInstantaneousSyntheticRDPrice(
+        uint256[] memory _lastBalancesWad
+    ) internal view returns (uint256 _syntheticRDPriceWad) {
+        StablePool _pool = StablePool(pool);
+        uint256 _numBalances = _lastBalancesWad.length;
+        (uint256 _ampValue, , uint256 _ampPrecision) = _pool.getAmplificationParameter();
+        uint256 _poolInvariant = _pool.computeInvariant(_lastBalancesWad, Rounding.ROUND_UP);
+        uint256 _ampCoefficient = (_numBalances ** _numBalances) * _ampValue;
 
-        // Get price of RD in terms of USDC
-        uint256 amountOut_USDC_base = aggregatorRouter.querySwapSingleTokenExactIn(
-            pool, // Address of the liquidity pool
-            IERC20(RD), // Token to be swapped from
-            IERC20(USDC), // Token to be swapped to
-            1, // Exact amounts of input tokens to send
-            address(this), //  The sender passed to the operation
-            bytes("") // Additional data to pass to the operation
+        uint256 _balancesSum;
+        for (uint256 _i = 0; _i < _numBalances; _i++) {
+            _balancesSum += _lastBalancesWad[_i];
+        }
+
+        // Calculate partial derivative for RD
+        uint256 _derivativeRD = _calculatePartialDerivative(
+            _lastBalancesWad[rdTokenIndex],
+            _ampCoefficient,
+            _poolInvariant,
+            _balancesSum,
+            _ampPrecision
         );
 
-        // Get price of RD in terms of USDT
-        uint256 amountOut_USDT_base = aggregatorRouter.querySwapSingleTokenExactIn(
-            pool,
-            IERC20(RD),
-            IERC20(USDT),
-            1,
-            address(this),
-            bytes("")
-        );
+        // Calculate weighted sum of stablecoin prices in RD
+        uint256 _weightedSumStablePriceInRD;
+        // Calculate total stablecoin balance
+        uint256 _totalStableBalanceWad;
 
-        // Get price of RD in terms of DAI
-        uint256 amountOut_DAI_base = aggregatorRouter.querySwapSingleTokenExactIn(
-            pool,
-            IERC20(RD),
-            IERC20(DAI),
-            1,
-            address(this),
-            bytes("")
-        );
+        for (uint256 _i = 0; _i < stablecoinBasket.length; _i++) {
+            uint8 _stableIndex = _stablecoinBasketIndices[_i];
+            uint256 _stableBalanceWad = _lastBalancesWad[_stableIndex];
 
-        uint256 RD_DECIMALS = 18;
-        uint256 INTERNAL_PRECISION = 1e18;
+            // Calculate partial derivative for this stablecoin
+            uint256 _derivativeStablecoin = _calculatePartialDerivative(
+                _stableBalanceWad,
+                _ampCoefficient,
+                _poolInvariant,
+                _balancesSum,
+                _ampPrecision
+            );
 
-        // 2. Get CL price of each other token in the pool
+            // Price of stablecoin _i in terms of RD = _derivativeRD / _derivativeStablecoin
+            uint256 _priceStableInRD = _derivativeRD.divDown(_derivativeStablecoin);
 
-        // Get USDC Chainlink price
-        (, int256 _USDC_feedResult, , uint256 _USDC_feedTimestamp, ) = AggregatorV3Interface(
-            USDC_CL_FEED
-        ).latestRoundData();
+            _weightedSumStablePriceInRD += _priceStableInRD.mulDown(_stableBalanceWad);
 
-        // Get USDT Chainlink price
-        (, int256 _USDT_feedResult, , uint256 _USDT_feedTimestamp, ) = AggregatorV3Interface(
-            USDT_CL_FEED
-        ).latestRoundData();
+            _totalStableBalanceWad += _stableBalanceWad;
+        }
+        if (_totalStableBalanceWad == 0) {
+            revert Oracle_DivisionByZero();
+        }
 
-        // Get DAI Chainlink price
-        (, int256 _DAI_feedResult, , uint256 _DAI_feedTimestamp, ) = AggregatorV3Interface(
-            DAI_CL_FEED
-        ).latestRoundData();
+        // Average price of the stablecoin basket unit, measured in RD
+        _avgBasketPriceInRD = _weightedSumStablePriceInRD.divDown(_totalStableBalanceWad);
 
-        // 3. Calculate USD price of RD in terms of each other token in the pool
+        if (_avgBasketPriceInRD == 0) {
+            revert Oracle_DivisionByZero();
+        }
 
-        uint256 num_usdc = amountOut_USDC_base *
-            uint256(_USDC_feedResult) *
-            (10 ** RD_DECIMALS) *
-            INTERNAL_PRECISION;
-        uint256 den_usdc = uint256(10 ** 8) * uint256(10 ** USDC_DECIMALS);
-        uint256 priceRD_via_USDC_scaled = (den_usdc == 0) ? 0 : num_usdc / den_usdc;
+        // Synthetic Price RD/USD = 1 / avgBasketPriceInRD
+        // Use WAD * WAD / x for 1/x equivalent, preserves WAD scaling
+        _syntheticRDPriceWad = WAD.mulDown(WAD).divDown(_avgBasketPriceInRD);
 
-        uint256 num_usdt = amountOut_USDT_base *
-            uint256(_USDT_feedResult) *
-            (10 ** RD_DECIMALS) *
-            INTERNAL_PRECISION;
-        uint256 den_usdt = uint256(10 ** 8) * uint256(10 ** USDT_DECIMALS);
-        uint256 priceRD_via_USDT_scaled = (den_usdt == 0) ? 0 : num_usdt / den_usdt;
+        // Average price of the stablecoin basket unit, measured in RD
+        uint256 _avgBasketPriceInRD = _weightedSumStablePriceInRD.divDown(_totalStableBalanceWad);
+    }
 
-        uint256 num_dai = amountOut_DAI_base *
-            uint256(_DAI_feedResult) *
-            (10 ** RD_DECIMALS) *
-            INTERNAL_PRECISION;
-        uint256 den_dai = uint256(10 ** 8) * uint256(10 ** DAI_DECIMALS);
-        uint256 priceRD_via_DAI_scaled = (den_dai == 0) ? 0 : num_dai / den_dai;
+    /**
+     * @notice Calculate the partial derivative of the stable pool invariant for a given token
+     * @dev    See Balancer v3 Stable Math Resources:
+     *
+     *         - https://docs.balancer.fi/concepts/explore-available-balancer-pools/stable-pool/stable-math.html#overview
+     *         - https://github.com/georgeroman/balancer-v2-pools/blob/main/src/pools/stable/math.ts#L16
+     *         - https://berkeley-defi.github.io/assets/material/StableSwap.pdf
+     *
+     * @dev
+     *
+     *         D = invariant (a measure of the total value in the pool)
+     *         A = amplification coefficient
+     *         S = sum of all token balances (x_1 + x_2 + ... + x_n)
+     *         P = product of all token balances (x_1 * x_2 * ... * x_n)
+     *         n = number of tokens
+     *
+     *         The StableSwap invariant equation is:
+     *
+     *         A * n^n * S + D = A * n^n * D + D^(n+1) / (n^n * P)
+     *
+     *         The derivative formula is:
+     *
+     *           df                  D^(n+1)                 1
+     *         ------ = n^n * A + ------------- = n^n * A + --- * (n^n * A * S + D - n^n * A * D)
+     *           dx                n^n * x * P               x
+     *
+     *         From the StableSwap invariant equation we isolate D^(n+1) / (n^n * P) by moving A * n^n * D to the left
+     *         side of the equation, which gives us the following identity:
+     *
+     *         D^(n+1) / (n^n * P) = A * n^n * S + D - A * n^n * D
+     *
+     *         We can then rewrite the second term of the derivative formula as:
+     *
+     *         [ D^(n+1) / (n^n * P) ] * (1/x)
+     *
+     *         We then substitute the previously derived identity into the second term of the derivative formula:
+     *
+     *         df/dx = n^n * A + [ A * n^n * S + D - A * n^n * D ] * (1/x)
+     *
+     *         df/dx = n^n * A + (A * n^n * S + D - A * n^n * D) / x
+     *
+     *
+     * @param  _tokenBalance The balance of the token
+     * @param  _ampCoefficient The amplification coefficient (n^n * A)
+     * @param  _poolInvariant The pool invariant (D)
+     * @param  _balancesSum The sum of the balances of the pool (S)
+     * @param  _ampPrecision The precision of the amplification coefficient
+     * @return _partialDerivative The partial derivative for the token
+     */
+    function _calculatePartialDerivative(
+        uint256 _tokenBalance,
+        uint256 _ampCoefficient,
+        uint256 _poolInvariant,
+        uint256 _balancesSum,
+        uint256 _ampPrecision
+    ) internal pure returns (uint256 _partialDerivative) {
+        if (_balancesSum == 0) {
+            revert Oracle_DivisionByZero();
+        }
 
-        // 4. Calculate final RD price in USD
+        // The amplification parameter A is a dimensionless number that controls how
+        // closely the pool's behavior mimics a constant-sum invariant (like x+y=k)
+        // versus a constant-product invariant (like x*y=k)
 
-        uint256 final_RD_Price_Scaled = (priceRD_via_USDC_scaled +
-            priceRD_via_USDT_scaled +
-            priceRD_via_DAI_scaled) / 3;
+        // _ampPrecision is a scaling factor that ensures the amplification coefficient
+        // is represented in a fixed-point format (e.g. no decimals)
+
+        // The amplification value stored in the contract is scaled by _ampPrecision
+        // so we need to scale it down to WAD precision when calculating the partial
+        // derivative
+        //
+        // True A value = A value in contract / _ampPrecision
+
+        // Term 1 = n^n * A = _ampCoefficient
+        // _ampCoefficient * (WAD / _ampPrecision) -> A is scaled to WAD precision
+        uint256 term1 = _ampCoefficient.mulDown(WAD, _ampPrecision);
+
+        // Term 2 Total = (A * n^n * S + D - A * n^n * D) / x
+        // Term 2 Numerator = A * n^n * S + D - A * n^n * D
+        // Term 2 Numerator Part 1 = A * n^n * S
+        // Term 2 Numerator Part 2 = D
+        // Term 2 Numerator Part 3 = A * n^n * D
+
+        uint256 term2NumPart1 = _ampCoefficient.mulDown(_balancesSum, _ampPrecision);
+        uint256 term2NumPart2 = _poolInvariant;
+        uint256 term2NumPart3 = _ampCoefficient.mulDown(_poolInvariant, _ampPrecision);
+
+        uint256 term2Numerator = term2NumPart1 + term2NumPart2 - term2NumPart3;
+
+        // Term 2 = term2Numerator / _tokenBalance
+        uint256 term2 = term2Numerator.divDown(_tokenBalance);
+
+        return term1 + term2;
     }
 }
