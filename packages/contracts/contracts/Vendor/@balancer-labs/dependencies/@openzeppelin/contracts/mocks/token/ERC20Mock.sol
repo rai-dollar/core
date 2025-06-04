fbb8694a12fd0a8ae69fb3edbeda61499271dc17
Luke Hamilton <pies.eth@pm.me>
2025-06-03 23:13:55 -0400

    chore: add additional balancer contracts

--- packages/contracts/contracts/Vendor/@balancer-labs/dependencies/@openzeppelin/contracts/mocks/token/ERC20Mock.sol
+++ packages/contracts/contracts/Vendor/@balancer-labs/dependencies/@openzeppelin/contracts/mocks/token/ERC20Mock.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {ERC20} from "../../token/ERC20/ERC20.sol";
 
-contract ERC20Mock is ERC20 {
+contract BalancerOZERC20Mock is ERC20 {
     constructor() ERC20("ERC20Mock", "E20M") {}
 
     function mint(address account, uint256 amount) external {
