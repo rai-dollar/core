f7751ca7b4b296490eb034cd123a0d8d0438d80e
MrDeadCe11 <mister.deadce11@gmail.com>
2025-07-01 23:40:56 -0500

    unit tests set up

--- packages/contracts/test/PriceFeedTest/PriceFeedBaseTest.js
+++ packages/contracts/test/PriceFeedTest/PriceFeedBaseTest.js
@@ -16,7 +16,7 @@ const hre = require("hardhat");
 // test with:
 // GAS_PRICE=70832172907 npx hardhat test test/PriceFeedTest/MainnetPriceFeedTest.js --config hardhat.config.mainnet-fork.js
 
-contract('PriceFeedFork', async accounts => {
+contract('PriceFeedUnitTests', async accounts => {
     const [owner, alice, bob, carol] = accounts;
     const [bountyAddress, lpRewardsAddress, multisig] = accounts.slice(997, 1000)
 
@@ -30,71 +30,81 @@ contract('PriceFeedFork', async accounts => {
         tellor: TimeValues.SECONDS_IN_ONE_DAY,  // 24hr staleness
     }
     const oracleConfigType = "OracleConfig(address primaryOracle, address fallbackOracle, uint256 primaryStalenessThreshold, uint256 fallbackStalenessThreshold)";
+     
+    let mockMarketAggregator;
+    let mockFallbackAggregator;
 
-    let priceFeed;
-    let mockAggregator;
+    let mockWstETH;
+
+    let priceFeedBase;
+
+    let blockTimestamp;
     console.log("BEFORE")
 
     before(async () => {
-        const marketOracleConfig = {
-            primaryOracle: chainlinkOracles.stEthUsd,
-            fallbackOracle: hre.ethers.constants.AddressZero,
-            primaryStalenessThreshold: stalenessThreshold.chainlink,
-            fallbackStalenessThreshold: stalenessThreshold.chainlink
-        }
 
+        blockTimestamp = await hre.ethers.provider.getBlock("latest").then(block => block.timestamp);
         const deployerWallet = (await ethers.getSigners())[0];
-        const deployerWalletAddress = deployerWallet.address;
 
-        const MockWSTETHPriceFeedFactory = await ethers.getContractFactory("MockAggregator", deployerWallet);
+        const MockChainlinkAggregator = await ethers.getContractFactory("MockAggregator", deployerWallet);
 
-        const mockMarketAggregator = await MockWSTETHPriceFeedFactory.deploy();
-        const mockEthAggregator = await MockWSTETHPriceFeedFactory.deploy();
+        mockMarketAggregator = await MockChainlinkAggregator.deploy();
+        mockFallbackAggregator = await MockChainlinkAggregator.deploy();
 
+        const MockWstETH = await ethers.getContractFactory("MockWstETH", deployerWallet);
+        mockWstETH = await MockWstETH.deploy();
 
-        console.log("Deploying WSTETHPriceFeed...");
-        const WSTETHPriceFeedFactory = await ethers.getContractFactory("PriceFeedBase", deployerWallet);
-        // deploy price feeds
-        wstEthPriceFeed = await WSTETHPriceFeedFactory.deploy(
-            stethMarketOracleConfig,
-            ethUsdOracleConfig,
-            tokens.wsteth,
-            tokens.wsteth,
-            deviationThreshold,
-            stalenessThreshold.chainlink
-        )
+        // set chainlink price to 8 decimals and api3 to 18 decimals
+        mockMarketAggregator.setPrice(hre.ethers.utils.parseUnits("1000", 8));
+        mockFallbackAggregator.setPrice(hre.ethers.utils.parseUnits("2000", 18));
 
-        mockedWstEthPriceFeed = await WSTETHPriceFeedFactory.deploy(
-            mockMarketAggregator,
-            mockEthAggregator,
-            tokens.wsteth,
-            tokens.wsteth,
-            deviationThreshold,
-            stalenessThreshold.chainlink
+        // set last updated time to 100 seconds ago
+        mockMarketAggregator.setUpdateTime(blockTimestamp - 100);
+        mockFallbackAggregator.setUpdateTime(blockTimestamp - 100);
+
+        mockMarketAggregator.setDecimals(8);
+        mockFallbackAggregator.setDecimals(18);
+
+        const stalenessThreshold = 1000;
+
+        const marketOracleConfig = {
+            primaryOracle: mockMarketAggregator.address,
+            fallbackOracle: mockFallbackAggregator.address,
+            primaryStalenessThreshold: stalenessThreshold,
+            fallbackStalenessThreshold: stalenessThreshold
+        }
+        // get block timestamp
+        const block = await hre.ethers.provider.getBlock("latest");
+        blockTimestamp = block.timestamp;
+
+        const priceFeedBaseFactory = await ethers.getContractFactory("PriceFeedBaseTester", deployerWallet);
+
+        priceFeedBase = await priceFeedBaseFactory.deploy(
+            marketOracleConfig,
+            mockWstETH.address,
+            deviationThreshold
         );
-        console.log("WSTETHPriceFeed deployed at:", wstEthPriceFeed.address);
+
     })
 
-    function getPriceWstEthUsdFromReceipt(receipt) {
-        const price = receipt.events.filter(event => event.event === "WstEthUsdResponseSaved")[0].args.price;
+    function getPriceFromReceipt(receipt) {
+        const price = receipt.events.filter(event => event.event === "LastGoodMarketResponseUpdated")[0].args.price;
         return parseFloat(hre.ethers.utils.formatEther(price));
     }
     describe("PriceFeedBase", () => {
 
-        it("should get the price of wsteth in usd", async () => {
-            // Use fetchPrice instead of getPrice
-            const tx = await wstEthPriceFeed.fetchPrice(false);
+        it("should get the primary oracle price", async () => {
+            const tx = await priceFeedBase.fetchPrice(false);
             const receipt = await tx.wait();
-            const price = getPriceWstEthUsdFromReceipt(receipt);
-            // Add some assertions
-            expect(price).to.be.greaterThan(0).and.lessThan(parseFloat(hre.ethers.utils.parseEther("5")));
+            const price = getPriceFromReceipt(receipt);
+            expect(parseFloat(price)).to.equal(1000);
         });
         
         it("should use fallback oracle if primary oracle is stale", async () => {
-            const tx = await mockedWstEthPriceFeed.fetchPrice(false);
+            const tx = await priceFeedBase.fetchPrice(false);
             const receipt = await tx.wait();
-            const price = getPriceWstEthUsdFromReceipt(receipt);
-            expect(price).to.be.greaterThan(0).and.lessThan(parseFloat(hre.ethers.utils.parseEther("5")));
+            const price = getPriceFromReceipt(receipt);
+            expect(parseFloat(price)).to.equal(1000);
         });
 
     })
